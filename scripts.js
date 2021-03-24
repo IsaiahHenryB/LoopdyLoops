d2ee8bff502463bc103c50c0328f91d112c1d40b
IsaiahHenry <isaiahhenryb@gmail.com>
2021-03-23 21:41:35 -0400

    complete

---
+++ scripts.js
@@ -0,0 +1,66 @@
+// const zeroToTen = () =>{
+//     for(let i=0; i<=10; i++){
+//         console.log(`the number is ${i}`)
+//     }
+// }
+// zeroToTen();
+
+// const fiveToFifteen = () =>{
+//     for(let i=5; i<=15; i++){
+//         if(i % 3 === 0)
+//         console.log(`${i} is a multiple of 3`)
+//     }
+// }
+// fiveToFifteen()
+
+// const fortyToSixty = (num1, num2) =>{
+//     for(let i=num1; i<=num2; i++){
+//         if(i % 2 === 0){
+//         console.log(i);
+//         }
+//         else{
+//             console.log('\n')
+//         }
+//     }
+// }
+// // WYM BY ALERT THE RESULT ^^
+// fortyToSixty(40,60)
+
+// const AhunnitToZero = (num1, num2) =>{
+//     for(let i = num1; i>= num2; i--){
+//         console.log(i);
+//     }
+// }
+// AhunnitToZero(100, 0)
+
+// const times = (num1, num2)=>{
+//     console.log(num1*num2)
+// }
+// times(4,8)
+
+// const containsA = (str) =>{
+//     for(let i = 0; i<= str.length; i++){
+//         if(str[i] === "a" || str[i] === "A"){
+//             return true
+//         }
+//     }
+//     return false
+// }
+// console.log(containsA('abraham'))
+// console.log(containsA('buns'))
+
+// const firstLast = (str) => {
+//     for( let i = 0; i <= str.length-1; i++){
+//         if(i===0 || i === str.length-1){
+//         console.log(str[i])
+//     }
+//     }
+// }
+// firstLast("buns")
+
+// const timesTable = (num1) =>{
+//     for(let i=0; i<=10; i++){
+//         console.log(num1*i)
+//     }
+// }
+// timesTable(56)
